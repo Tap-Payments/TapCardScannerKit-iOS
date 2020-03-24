8c93d7f6503a155d0c6d9fcd164a378652d53c17
Tap Payments <o.rabie@tap.company>
2020-03-24 09:57:16 +0200

    1- Creating the framework empty project. 2- Adding empty scanner example project

---
+++ TapCardScanner-iOS/TapCardScanner-iOS/TapCardScanner_iOS.h
@@ -0,0 +1,19 @@
+//
+//  TapCardScanner_iOS.h
+//  TapCardScanner-iOS
+//
+//  Created by Osama Rabie on 24/03/2020.
+//  Copyright © 2020 Tap Payments. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TapCardScanner_iOS.
+FOUNDATION_EXPORT double TapCardScanner_iOSVersionNumber;
+
+//! Project version string for TapCardScanner_iOS.
+FOUNDATION_EXPORT const unsigned char TapCardScanner_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TapCardScanner_iOS/PublicHeader.h>
+
+
---
+++ TapCardScanner-iOS/TapCardScannerExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TapCardScannerExample
+//
+//  Created by Osama Rabie on 24/03/2020.
+//  Copyright © 2020 Tap Payments. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
