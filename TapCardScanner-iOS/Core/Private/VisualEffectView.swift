a85133d8b5b1aa78cc579445f371acb7188c8622
O Rabie <o.rabie@tap.company>
2020-07-11 08:14:21 +0200

    1- Create an extension for native blur effect to accept tint color

---
+++ TapCardScanner-iOS/Core/Private/VisualEffectView.swift
@@ -0,0 +1,94 @@
+//
+//  VisualEffectView.swift
+//  VisualEffectView
+//
+//  Created by Osama Rabie on 10/07/2020.
+//  Copyright © 2020 Tap Payments. All rights reserved.
+//
+
+import class UIKit.UIVisualEffectView
+import class UIKit.UIColor
+import struct UIKit.CGFloat
+import class UIKit.UIVisualEffect
+import class UIKit.UIBlurEffect
+
+/// VisualEffectView is a dynamic background blur view.
+internal class VisualEffectView: UIVisualEffectView {
+    
+    /// Returns the instance of UIBlurEffect.
+    private let blurEffect = (NSClassFromString("_UICustomBlurEffect") as! UIBlurEffect.Type).init()
+    
+    /**
+     Tint color.
+     
+     The default value is nil.
+     */
+    var colorTint: UIColor? {
+        get { return _value(forKey: "colorTint") as? UIColor }
+        set { _setValue(newValue, forKey: "colorTint") }
+    }
+    
+    /**
+     Tint color alpha.
+     
+     The default value is 0.0.
+     */
+    var colorTintAlpha: CGFloat {
+        get { return _value(forKey: "colorTintAlpha") as! CGFloat }
+        set { _setValue(newValue, forKey: "colorTintAlpha") }
+    }
+    
+    /**
+     Blur radius.
+     
+     The default value is 0.0.
+     */
+    var blurRadius: CGFloat {
+        get { return _value(forKey: "blurRadius") as! CGFloat }
+        set { _setValue(newValue, forKey: "blurRadius") }
+    }
+    
+    /**
+     Scale factor.
+     
+     The scale factor determines how content in the view is mapped from the logical coordinate space (measured in points) to the device coordinate space (measured in pixels).
+     
+     The default value is 1.0.
+     */
+    var scale: CGFloat {
+        get { return _value(forKey: "scale") as! CGFloat }
+        set { _setValue(newValue, forKey: "scale") }
+    }
+    
+    // MARK: - Initialization
+    
+    override init(effect: UIVisualEffect?) {
+        super.init(effect: effect)
+        
+        scale = 1
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        scale = 1
+    }
+    
+}
+
+// MARK: - Helpers
+
+private extension VisualEffectView {
+    
+    /// Returns the value for the key on the blurEffect.
+    func _value(forKey key: String) -> Any? {
+        return blurEffect.value(forKeyPath: key)
+    }
+    
+    /// Sets the value for the key on the blurEffect.
+    func _setValue(_ value: Any?, forKey key: String) {
+        blurEffect.setValue(value, forKeyPath: key)
+        self.effect = blurEffect
+    }
+    
+}
