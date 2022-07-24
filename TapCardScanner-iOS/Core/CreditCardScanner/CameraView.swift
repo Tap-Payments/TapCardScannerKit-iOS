94d41fef593924c23591d668bb667d8d43d4e614
O Rabie <o.rabie@tap.company>
2022-07-24 12:07:43 +0200

    Configure showing blurred background with the full screen scanner.

--- TapCardScanner-iOS/Core/CreditCardScanner/CameraView.swift
+++ TapCardScanner-iOS/Core/CreditCardScanner/CameraView.swift
@@ -22,21 +22,22 @@ protocol CameraViewDelegate: AnyObject {
 final class CameraView: UIView {
     weak var delegate: CameraViewDelegate?
     private var creditCardFrameStrokeColor: UIColor
+    private var showBlur: Bool = true
     private let maskLayerColor: UIColor
     private let maskLayerAlpha: CGFloat
-
+    
     // MARK: - Capture related
-
+    
     private let captureSessionQueue = DispatchQueue(
         label: "com.yhkaplan.credit-card-scanner.captureSessionQueue"
     )
-
+    
     // MARK: - Capture related
-
+    
     private let sampleBufferQueue = DispatchQueue(
         label: "com.yhkaplan.credit-card-scanner.sampleBufferQueue"
     )
-
+    
     init(
         delegate: CameraViewDelegate? = nil,
         creditCardFrameStrokeColor: UIColor = .green,
@@ -49,28 +50,28 @@ final class CameraView: UIView {
         self.maskLayerAlpha = maskLayerAlpha
         super.init(frame: .zero)
     }
-
+    
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     private let imageRatio: ImageRatio = .vga640x480
-
+    
     // MARK: - Region of interest and text orientation
-
+    
     /// Region of video data output buffer that recognition should be run on.
     /// Gets recalculated once the bounds of the preview layer are known.
     var regionOfInterest: CGRect?
-
+    
     var videoPreviewLayer: AVCaptureVideoPreviewLayer {
         guard let layer = layer as? AVCaptureVideoPreviewLayer else {
             fatalError("Expected `AVCaptureVideoPreviewLayer` type for layer. Check PreviewView.layerClass implementation.")
         }
-
+        
         return layer
     }
-
+    
     private var videoSession: AVCaptureSession? {
         get {
             videoPreviewLayer.session
@@ -79,40 +80,42 @@ final class CameraView: UIView {
             videoPreviewLayer.session = newValue
         }
     }
-
+    
     let semaphore = DispatchSemaphore(value: 1)
-
+    
     override class var layerClass: AnyClass {
         AVCaptureVideoPreviewLayer.self
     }
-
+    
     func stopSession() {
         videoSession?.stopRunning()
     }
-
+    
     func startSession() {
         videoSession?.startRunning()
     }
-
+    
     func setupCamera(with uiCustomization:TapFullScreenUICustomizer = .init()) {
         creditCardFrameStrokeColor = uiCustomization.tapFullScreenScanBorderColor
+        showBlur = uiCustomization.blurCardScannerBackground
+        
         captureSessionQueue.async { [weak self] in
             self?._setupCamera()
         }
     }
-
+    
     private func _setupCamera() {
         let session = AVCaptureSession()
         session.beginConfiguration()
         session.sessionPreset = imageRatio.preset
-
+        
         guard let videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera,
                                                         for: .video,
                                                         position: .back) else {
             delegate?.didError(with: "Error setup camera")
             return
         }
-
+        
         do {
             let deviceInput = try AVCaptureDeviceInput(device: videoDevice)
             session.canAddInput(deviceInput)
@@ -120,80 +123,85 @@ final class CameraView: UIView {
         } catch {
             delegate?.didError(with: error)
         }
-
+        
         let videoOutput = AVCaptureVideoDataOutput()
         videoOutput.alwaysDiscardsLateVideoFrames = true
         videoOutput.setSampleBufferDelegate(self, queue: sampleBufferQueue)
-
+        
         guard session.canAddOutput(videoOutput) else {
             delegate?.didError(with: "Error setup camera")
             return
         }
-
+        
         session.addOutput(videoOutput)
         session.connections.forEach {
             $0.videoOrientation = .portrait
         }
         session.commitConfiguration()
-
+        
         DispatchQueue.main.async { [weak self] in
             self?.videoPreviewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
             self?.videoSession = session
             self?.startSession()
         }
     }
-
+    
     func setupRegionOfInterest() {
         guard regionOfInterest == nil else { return }
         /// Mask layer that covering area around camera view
         /*let backLayer = CALayer()
-        backLayer.frame = bounds
-        if let blurFilter = CIFilter(name: "CIGaussianBlur",
-                                     parameters: [kCIInputRadiusKey: 2]) {
-            backLayer.backgroundFilters = [blurFilter]
-        }
-        backLayer.backgroundColor = maskLayerColor.withAlphaComponent(maskLayerAlpha).cgColor*/
-
-        /*let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.prominent)
+         backLayer.frame = bounds
+         if let blurFilter = CIFilter(name: "CIGaussianBlur",
+         parameters: [kCIInputRadiusKey: 2]) {
+         backLayer.backgroundFilters = [blurFilter]
+         }
+         backLayer.backgroundColor = maskLayerColor.withAlphaComponent(maskLayerAlpha).cgColor*/
+        
+        let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.prominent)
         let blurEffectView = UIVisualEffectView(effect: blurEffect)
         blurEffectView.frame = bounds
         blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        addSubview(blurEffectView)*/
+        
+        if showBlur {
+            addSubview(blurEffectView)
+        }
         
         //  culcurate cutoutted frame
         let cuttedWidth: CGFloat = bounds.width - 40.0
         let cuttedHeight: CGFloat = cuttedWidth * TapCard.heightRatioAgainstWidth
-
+        
         let centerVertical = (bounds.height / 2.0)
         let cuttedY: CGFloat = centerVertical - (cuttedHeight / 2.0)
         let cuttedX: CGFloat = 20.0
-
+        
         let cuttedRect = CGRect(x: cuttedX,
                                 y: cuttedY,
                                 width: cuttedWidth,
                                 height: cuttedHeight)
-
         
-        /*let maskLayer = CAShapeLayer()
-        let path = UIBezierPath(roundedRect: cuttedRect, cornerRadius: 10.0)
-
-        path.append(UIBezierPath(rect: bounds))
-        maskLayer.path = path.cgPath
-        maskLayer.fillRule = .evenOdd
-        //backLayer.mask = maskLayer
-        //layer.addSublayer(backLayer)
-        blurEffectView.layer.mask = maskLayer*/
-
+        
+        if showBlur {
+            let maskLayer = CAShapeLayer()
+            let path = UIBezierPath(roundedRect: cuttedRect, cornerRadius: 10.0)
+            
+            path.append(UIBezierPath(rect: bounds))
+            maskLayer.path = path.cgPath
+            maskLayer.fillRule = .evenOdd
+            //backLayer.mask = maskLayer
+            //layer.addSublayer(backLayer)
+            blurEffectView.layer.mask = maskLayer
+        }
+        
         let strokeLayer = CAShapeLayer()
         strokeLayer.lineWidth = 1.0
         strokeLayer.strokeColor = creditCardFrameStrokeColor.cgColor
         strokeLayer.path = UIBezierPath(roundedRect: cuttedRect, cornerRadius: 10.0).cgPath
         strokeLayer.fillColor = nil
         layer.addSublayer(strokeLayer)
-
+        
         let imageHeight: CGFloat = imageRatio.imageHeight
         let imageWidth: CGFloat = imageRatio.imageWidth
-
+        
         let acutualImageRatioAgainstVisibleSize = imageWidth / bounds.width
         let interestX = cuttedRect.origin.x * acutualImageRatioAgainstVisibleSize
         let interestWidth = cuttedRect.width * acutualImageRatioAgainstVisibleSize
@@ -205,9 +213,9 @@ final class CameraView: UIView {
                                   height: interestHeight)
         
         /*let dummyView:UIView = .init(frame: cuttedRect)
-        dummyView.backgroundColor = .red
-        addSubview(dummyView)
-                                     print(dummyView.frame)*/
+         dummyView.backgroundColor = .red
+         addSubview(dummyView)
+         print(dummyView.frame)*/
     }
 }
 
@@ -216,27 +224,27 @@ extension CameraView: AVCaptureVideoDataOutputSampleBufferDelegate {
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
         semaphore.wait()
         defer { semaphore.signal() }
-
+        
         guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
             delegate?.didError(with: "Error capture camera")
             delegate = nil
             return
         }
-
+        
         var cgImage: CGImage?
         VTCreateCGImageFromCVPixelBuffer(pixelBuffer, options: nil, imageOut: &cgImage)
-
+        
         guard let regionOfInterest = regionOfInterest else {
             return
         }
-
+        
         guard let fullCameraImage = cgImage,
-            let _ = fullCameraImage.cropping(to: regionOfInterest) else {
+              let _ = fullCameraImage.cropping(to: regionOfInterest) else {
             delegate?.didError(with: "Error capture camera")
             delegate = nil
             return
         }
-
+        
         delegate?.didCapture(image: fullCameraImage)
     }
 }
--- TapCardScanner-iOS/Core/Public/TapFullScreenScannerViewController.swift
+++ TapCardScanner-iOS/Core/Public/TapFullScreenScannerViewController.swift
@@ -22,20 +22,20 @@ import CommonDataModelsKit_iOS
 
 @available(iOS 13, *)
 @objc open class TapFullScreenScannerViewController: UIViewController {
-
+    
     /*public var titleLabelText: String = "Add card"
-    public var subtitleLabelText: String = "Line up card within the lines"
-    public var cancelButtonTitleText: String = "Cancel"
-    public var cancelButtonTitleTextColor: UIColor = .gray
-    public var labelTextColor: UIColor = .white
-    public var textBackgroundColor: UIColor = .black*/
+     public var subtitleLabelText: String = "Line up card within the lines"
+     public var cancelButtonTitleText: String = "Cancel"
+     public var cancelButtonTitleTextColor: UIColor = .gray
+     public var labelTextColor: UIColor = .white
+     public var textBackgroundColor: UIColor = .black*/
     internal var textBackgroundColor: UIColor = .black
     internal var cameraViewCreditCardFrameStrokeColor: UIColor = .green
     internal var cameraViewMaskLayerColor: UIColor = .black
     internal var cameraViewMaskAlpha: CGFloat = 0.7
-
+    
     // MARK: - Subviews and layers
-
+    
     /// View representing live camera
     private lazy var cameraView: CameraView = CameraView(
         delegate: self,
@@ -43,13 +43,13 @@ import CommonDataModelsKit_iOS
         maskLayerColor: self.cameraViewMaskLayerColor,
         maskLayerAlpha: self.cameraViewMaskAlpha
     )
-
+    
     /// Analyzes text data for credit card info
     private lazy var analyzer = ImageAnalyzer(delegate: self)
     
     /// Conform to this delegate to get notified of key events
     @objc public weak var delegate: TapCreditCardScannerViewControllerDelegate?
-
+    
     /// uiCustomization: The UI customization object to theme the scanner
     private var uiCustomization:TapFullScreenUICustomizer = .init()
     
@@ -58,21 +58,21 @@ import CommonDataModelsKit_iOS
     private var titleLabel = UILabel()
     private var subtitleLabel = UILabel()
     private var cancelButton = UIButton(type: .system)
-
+    
     // MARK: - Vision-related
-
+    
     /// - Parameter delegate: Conform to this delegate to get notified of key events
     /// - Parameter uiCustomization: The UI customization object to theme the scanner
     public init(delegate: TapCreditCardScannerViewControllerDelegate? = nil, uiCustomization:TapFullScreenUICustomizer = .init()) {
         self.delegate = delegate
         super.init(nibName: nil, bundle: nil)
     }
-
+    
     @available(*, unavailable)
     public required init?(coder: NSCoder) {
         fatalError("Not implemented")
     }
-
+    
     override open func viewDidLoad() {
         super.viewDidLoad()
         layoutSubviews()
@@ -92,7 +92,7 @@ import CommonDataModelsKit_iOS
             strongSelf.cameraView.setupCamera(with: strongSelf.uiCustomization)
         }
     }
-
+    
     override open func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         cameraView.setupRegionOfInterest()
@@ -104,7 +104,7 @@ private extension TapFullScreenScannerViewController {
     @objc func cancel(_ sender: UIButton) {
         delegate?.creditCardScannerViewControllerDidCancel(self)
     }
-
+    
     func layoutSubviews() {
         view.backgroundColor = textBackgroundColor
         // TODO: test screen rotation cameraView, cutoutView
@@ -117,46 +117,46 @@ private extension TapFullScreenScannerViewController {
             cameraView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             //cameraView.heightAnchor.constraint(equalTo: cameraView.widthAnchor, multiplier: CreditCard.heightRatioAgainstWidth, constant: 100),
         ])
-
+        
         /*bottomStackView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(bottomStackView)
-        NSLayoutConstraint.activate([
-            bottomStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            bottomStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            bottomStackView.topAnchor.constraint(equalTo: cameraView.bottomAnchor),
-        ])
-
-        cancelButton.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(cancelButton)
-        NSLayoutConstraint.activate([
-            cancelButton.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            cancelButton.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            cancelButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
-        ])
-
-        bottomStackView.axis = .vertical
-        bottomStackView.spacing = 16.0
-        bottomStackView.isLayoutMarginsRelativeArrangement = true
-        bottomStackView.distribution = .equalSpacing
-        bottomStackView.directionalLayoutMargins = .init(top: 8.0, leading: 8.0, bottom: 8.0, trailing: 8.0)
-        let arrangedSubviews: [UIView] = [titleLabel, subtitleLabel]
-        arrangedSubviews.forEach(bottomStackView.addArrangedSubview)*/
+         view.addSubview(bottomStackView)
+         NSLayoutConstraint.activate([
+         bottomStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+         bottomStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+         bottomStackView.topAnchor.constraint(equalTo: cameraView.bottomAnchor),
+         ])
+         
+         cancelButton.translatesAutoresizingMaskIntoConstraints = false
+         view.addSubview(cancelButton)
+         NSLayoutConstraint.activate([
+         cancelButton.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+         cancelButton.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+         cancelButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+         ])
+         
+         bottomStackView.axis = .vertical
+         bottomStackView.spacing = 16.0
+         bottomStackView.isLayoutMarginsRelativeArrangement = true
+         bottomStackView.distribution = .equalSpacing
+         bottomStackView.directionalLayoutMargins = .init(top: 8.0, leading: 8.0, bottom: 8.0, trailing: 8.0)
+         let arrangedSubviews: [UIView] = [titleLabel, subtitleLabel]
+         arrangedSubviews.forEach(bottomStackView.addArrangedSubview)*/
     }
-
+    
     /*func setupLabelsAndButtons() {
-        titleLabel.text = titleLabelText
-        titleLabel.textAlignment = .center
-        titleLabel.textColor = labelTextColor
-        titleLabel.font = .preferredFont(forTextStyle: .largeTitle)
-        subtitleLabel.text = subtitleLabelText
-        subtitleLabel.textAlignment = .center
-        subtitleLabel.font = .preferredFont(forTextStyle: .title3)
-        subtitleLabel.textColor = labelTextColor
-        subtitleLabel.numberOfLines = 0
-        cancelButton.setTitle(cancelButtonTitleText, for: .normal)
-        cancelButton.setTitleColor(cancelButtonTitleTextColor, for: .normal)
-        cancelButton.addTarget(self, action: #selector(cancel), for: .touchUpInside)
-    }*/
+     titleLabel.text = titleLabelText
+     titleLabel.textAlignment = .center
+     titleLabel.textColor = labelTextColor
+     titleLabel.font = .preferredFont(forTextStyle: .largeTitle)
+     subtitleLabel.text = subtitleLabelText
+     subtitleLabel.textAlignment = .center
+     subtitleLabel.font = .preferredFont(forTextStyle: .title3)
+     subtitleLabel.textColor = labelTextColor
+     subtitleLabel.numberOfLines = 0
+     cancelButton.setTitle(cancelButtonTitleText, for: .normal)
+     cancelButton.setTitleColor(cancelButtonTitleTextColor, for: .normal)
+     cancelButton.addTarget(self, action: #selector(cancel), for: .touchUpInside)
+     }*/
 }
 
 @available(iOS 13, *)
@@ -164,7 +164,7 @@ extension TapFullScreenScannerViewController: CameraViewDelegate {
     internal func didCapture(image: CGImage) {
         analyzer.analyze(image: image)
     }
-
+    
     internal func didError(with error: Error) {
         DispatchQueue.main.async { [weak self] in
             guard let strongSelf = self else { return }
@@ -186,7 +186,7 @@ extension TapFullScreenScannerViewController: ImageAnalyzerProtocol {
                     strongSelf.delegate?.creditCardScannerViewController(strongSelf, didFinishWith: creditCard)
                 })
             }
-
+            
         case let .failure(error):
             DispatchQueue.main.async { [weak self] in
                 guard let strongSelf = self else { return }
@@ -205,14 +205,14 @@ extension AVCaptureDevice {
                 authorizedHandler(isAuthorized)
             }
         }
-
+        
         switch authorizationStatus(for: .video) {
         case .authorized:
             mainThreadHandler(true)
         case .notDetermined:
             requestAccess(for: .video, completionHandler: { granted in
                 mainThreadHandler(granted)
-})
+            })
         default:
             mainThreadHandler(false)
         }
@@ -230,13 +230,24 @@ extension AVCaptureDevice {
 @objcMembers public class TapFullScreenUICustomizer:NSObject {
     
     /// The cancel button title default is "Cancel"
-    public lazy var tapFullScreenCancelButtonTitle:String = "Cancel"
+    public var tapFullScreenCancelButtonTitle:String = "Cancel"
     /// The cancel button font default is system with 15
-    public lazy var tapFullScreenCancelButtonFont:UIFont  = UIFont.systemFont(ofSize: 15)
+    public var tapFullScreenCancelButtonFont:UIFont  = UIFont.systemFont(ofSize: 15)
     /// The cancel button color default is "White"
-    public lazy var tapFullScreenCancelButtonTitleColor:UIColor  = .white
+    public var tapFullScreenCancelButtonTitleColor:UIColor  = .white
     /// The cancel button holder background color default is "Black"
-    public lazy var tapFullScreenCancelButtonHolderViewColor:UIColor  = .black
+    public var tapFullScreenCancelButtonHolderViewColor:UIColor  = .black
     /// The borders of scan card color default is "Green"
-    public lazy var tapFullScreenScanBorderColor:UIColor  = .green
+    public var tapFullScreenScanBorderColor:UIColor  = .green
+    // Indicates whether ot not to show a blur background around the scanning area
+    public var blurCardScannerBackground:Bool = true
+    
+    public init(tapFullScreenCancelButtonTitle: String = "Cancel", tapFullScreenCancelButtonFont: UIFont = UIFont.systemFont(ofSize: 15), tapFullScreenCancelButtonTitleColor: UIColor = .white, tapFullScreenCancelButtonHolderViewColor: UIColor = .black, tapFullScreenScanBorderColor: UIColor = .green, blurCardScannerBackground: Bool = true) {
+        self.tapFullScreenCancelButtonTitle = tapFullScreenCancelButtonTitle
+        self.tapFullScreenCancelButtonFont = tapFullScreenCancelButtonFont
+        self.tapFullScreenCancelButtonTitleColor = tapFullScreenCancelButtonTitleColor
+        self.tapFullScreenCancelButtonHolderViewColor = tapFullScreenCancelButtonHolderViewColor
+        self.tapFullScreenScanBorderColor = tapFullScreenScanBorderColor
+        self.blurCardScannerBackground = blurCardScannerBackground
+    }
 }
